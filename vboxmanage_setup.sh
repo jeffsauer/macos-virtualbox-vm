3fc82eeae878b6a5872543e50142c071f0c84728
jeffsauer <jsauer@jouledata.com>
2019-03-08 15:27:15 -0700

    Add files via upload

--- vboxmanage_setup.sh
+++ vboxmanage_setup.sh
@@ -1,7 +1,13 @@
+#~/bin/sh
+
 vboxmanage modifyvm macOS_Mojave --cpuidset 00000001 000106e5 00100800 0098e3fd bfebfbff
 vboxmanage setextradata macOS_Mojave "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "iMac11,3"
 vboxmanage setextradata macOS_Mojave "VBoxInternal/Devices/efi/0/Config/DmiSystemVersion" "1.0"
 vboxmanage setextradata macOS_Mojave "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Iloveapple"
 vboxmanage setextradata macOS_Mojave "VBoxInternal/Devices/smc/0/Config/DeviceKey" "ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
 vboxmanage setextradata macOS_Mojave "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 1
+
+# Serial number is needed to make iMessages work
+vboxmanage setextradata macOS_Mojave "VBoxInternal/Devices/efi/0/Config/DmiSystemSerial" C02PH2GMGJJ44
+
 
